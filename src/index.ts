3c63301deff87a53a5d98becd742e40c5203b06d
Muhammad Omer Siddiqui <omersiddiqui67@gmail.com>
2024-06-02 20:10:31 +0500

    subscription

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import { PrismaClient } from "@prisma/client";
 import AuthRouter from './routes/AuthRoutes';
 import EducationRouter from './routes/EducationRoutes';
 import PostRouter from './routes/PostRoutes';
+import SubscriptionRouter from './routes/SubscriptionRoutes';
 import dotenv from "dotenv";
 import "express-async-errors";
 import bodyParser from "body-parser";
 import { errorHandler } from "./middlewares/error";
 import path from "path";
+import cors from "cors";
 
 dotenv.config();
 
@@ -19,6 +21,7 @@ const port = process.env.PORT || 3000;
 async function main() {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(express.json());
+  app.use(cors());
 
   app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
@@ -26,6 +29,8 @@ async function main() {
   app.use("/api/v1/auth", AuthRouter);
   app.use("/api/v1/education", EducationRouter);
   app.use("/api/v1/post", PostRouter)
+  app.use("/api/v1/subscription", SubscriptionRouter)
+
 
   app.use(errorHandler);
 
